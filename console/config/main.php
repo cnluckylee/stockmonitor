d645d4679898ec638607531281c1ae0abc12f6bf
Your Name <you@example.com>
2017-07-27 10:38:25 +0800

    ij

--- console/config/main.php
+++ console/config/main.php
@@ -14,30 +14,6 @@ return [
         'gii' => 'yii\gii\Module',
     ],
     'components' => [
-<<<<<<< HEAD
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'mongodb' => [
-            'class' => '\yii\mongodb\Connection',
-            'dsn' => 'mongodb://127.0.0.1:19879/jubi',
-        ],
-=======
-
->>>>>>> 7a7c61ea870a18d4326e5ff6ba3caa03129523e2
     ],
 
     'params' => $params,
