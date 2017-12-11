0bde2492a6cf9213dc689866ebe5f61941816f78
Your Name <you@example.com>
2017-12-11 23:12:37 +0800

    x

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,6 +1,15 @@
 <?php
 return [
     'components' => [
-
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://alialpha:19879/jubi',
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'alialpha',
+            'port' => 19889,
+            'database' => 1,
+        ],
     ],
 ];
